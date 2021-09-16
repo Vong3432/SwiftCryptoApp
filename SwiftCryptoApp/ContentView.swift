7a5c4e353df9f33f894b9ed8eba1d7f91665c120
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-09-17 01:15:41 +0800

    Initial Commit

---
+++ SwiftCryptoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCryptoApp
+//
+//  Created by Vong Nyuksoon on 17/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftCryptoApp/SwiftCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoAppApp.swift
+//  SwiftCryptoApp
+//
+//  Created by Vong Nyuksoon on 17/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
